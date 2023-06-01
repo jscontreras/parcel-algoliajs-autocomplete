6aceddd4c6d5981cdee81d828267384960a66e3d
Sebastian Contreras <sebastian.contreras@algolia.com>
2023-06-01 10:59:31 -0500

    plugins demo ready.

--- src/demo2/app.js
+++ src/demo2/app.js
@@ -13,39 +13,46 @@ const apiKey = '6be0576ff61c053d5f9a3225e2a90f76';
 const indexName = 'instant_search';
 const querySuggestionsIndex = 'instant_search_demo_query_suggestions';
 
-insightsClient('init', { appId, apiKey });
-const algoliaInsightsPlugin = createAlgoliaInsightsPlugin({
-  insightsClient,
-  onActive({ insights, insightsEvents, item, state, event }) {
-    insightsEvents.forEach((insightsEvent) => {
-      // Assuming you've initialized the Segment script
-      // and identified the current user already
-      console.log(
-        'SEGMENT (ACTIVE) Event Forwarding',
-        insightsEvents,
-        insights
-      );
-      // analytics.track('Product Browsed from Autocomplete', insightsEvent);
-    });
-  },
-  onItemsChange({ insights, insightsEvents, item, state, event }) {
-    // Assuming you've initialized the Segment script
-    // and identified the current user already
-    console.log('SEGMENT (CHANGE) Event Forwarding', insightsEvents, insights);
-    // analytics.track('Product Browsed from Autocomplete', insightsEvent);
-  },
-  onSelect({ insights, insightsEvents, item, state, event }) {
-    // Assuming you've initialized the Segment script
-    // and identified the current user already
-    console.log(
-      'SEGMENT (SELECT) Event Forwarding',
-      insightsEvents,
-      insights,
-      item
-    );
-    // analytics.track('Product Browsed from Autocomplete', insightsEvent);
-  },
+insightsClient('init', {
+  appId,
+  apiKey,
+  useCookie: false,
+  userToken: 'ma-user-999',
 });
+// const algoliaInsightsPlugin = createAlgoliaInsightsPlugin({
+//   insightsClient,
+//   onActive({ insights, insightsEvents, item, state, event }) {
+//     insightsEvents.forEach((insightsEvent) => {
+//       // Assuming you've initialized the Segment script
+//       // and identified the current user already
+//       console.log(
+//         'SEGMENT (ACTIVE) Event Forwarding',
+//         insightsEvents,
+//         insights
+//       );
+//       // analytics.track('Product Browsed from Autocomplete', insightsEvent);
+//     });
+//   },
+//   onItemsChange({ insights, insightsEvents, item, state, event }) {
+//     // Assuming you've initialized the Segment script
+//     // and identified the current user already
+//     console.log('SEGMENT (CHANGE) Event Forwarding', insightsEvents, insights);
+//     // analytics.track('Product Browsed from Autocomplete', insightsEvent);
+//   },
+//   onSelect({ insights, insightsEvents, item, state, event }) {
+//     // Assuming you've initialized the Segment script
+//     // and identified the current user already
+//     console.log(
+//       'SEGMENT (SELECT) Event Forwarding',
+//       insightsEvents,
+//       insights,
+//       item
+//     );
+//     // analytics.track('Product Browsed from Autocomplete', insightsEvent);
+//   },
+// });
+
+const algoliaInsightsPlugin = createAlgoliaInsightsPlugin({ insightsClient });
 
 const searchClient = algoliasearch(appId, apiKey);
 
@@ -66,15 +73,19 @@ const querySuggestionsPlugin = createQuerySuggestionsPlugin({
       },
     };
   },
+  getSearchParams({ state }) {
+    return {
+      query: state.query,
+      hitsPerPage: 2,
+    };
+  },
 });
 
 // Autcomplete Initialization
 autocomplete({
   openOnFocus: false,
   container: '#autocomplete',
-  // plugins: [algoliaInsightsPlugin],
   debug: true,
-  // plugins: [querySuggestionsPlugin, recentSearchesPlugin],
   plugins: [
     querySuggestionsPlugin,
     recentSearchesPlugin,
@@ -102,10 +113,10 @@ autocomplete({
         templates: {
           item({ item, components }) {
             return (
-              <div class="aa-ItemWrapper">
-                <div class="aa-ItemContent">
-                  <div class="aa-ItemContentBody">
-                    <div class="aa-ItemContentTitle">
+              <div className="aa-ItemWrapper">
+                <div className="aa-ItemContent">
+                  <div className="aa-ItemContentBody">
+                    <div className="aa-ItemContentTitle">
                       <components.Highlight hit={item} attribute="name" />
                     </div>
                   </div>
